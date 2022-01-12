f2695607ee7677f49d13b625be27a69918bbd0a4
Eliuds <cmeliud2003@gmail.com>
2022-01-12 00:44:48 -0500

    asd

---
+++ Basic JavaScript Projects/Project2_functions/JS/Jva.js
@@ -0,0 +1,11 @@
+function my_first_function() {
+    var str="Hi everyone";
+    var result=str.fontcolor("blue");
+document.getElementById("Blue").innerHTML=result;// everytime the id Blue is called it will display "hi everyone" in blue
+}
+
+function myfunction() {
+    var sentence = "i am learning";
+    sentence += "a lot from this";
+    document.getElementById("BigB").innerHTML= sentence; // everytime element BigB is called uppon it will display i am learning allot from this
+}
