217ad877667f31d48080dbb047d4fe9dd3540234
Eliuds <cmeliud2003@gmail.com>
2022-01-12 00:09:38 -0500

    assssss

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,15 @@
+window.alert("document");
+var a="This is a string";
+document.write(a);
+var b="this is another string value"
+document.write(b);
+document.write("Valorant is a good game,\"apex might be a better game\" he said.but at the end of the day theyre both good games");
+document.write("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. seuss");
+var c ="Pokemon" + "String" // giving the variable c a value
+document.write(c);// writing the var c
+var d="hi", e="hola", f="asd";// assigning values to variables
+var e= e.fontcolor("red");// making the var e red
+document.write(e);//writing the var e
+document.write(15+2);// im adding 15 and 2
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JavaScript-Projects
+This is gonna be where i place all the projects i do in the javascript course.
